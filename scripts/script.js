62bdc35525989ba26c71fc724e0a4dccf1204456
LyubovM11 <mazurovalubov8@gmail.com>
2025-04-09 13:08:47 +0300

    Add files via upload
    Завершили создание динамической составляющей "было/стало"!

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,27 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал правильно')
 });
 
-const loadContent = () => {
-    contentSection.innerHTML = '<p>This is dynamic content loaded with JavaScript.</p>';
-};
+// * 1. Начало
+// * 2. Получаем картинки и полузнок
+// * 3. Перемещаем ползунок
+// *  3.1. Перемещаем ползунок вправо 
+// *   3.1.1. Картинка "after" расширяется влево, а картинка "before" обрезается справа
+// *  3.2. Перемещаем ползунок влево
+// *   3.2.1. Картинка "before" расширяется вправо, а картинка "after" обрезается слева
+// * 4. Конец
 
-loadContent();
+let beforeRangeValue = 50;
+
+const catRange = document.querySelector(".input-range") 
+
+catRange.addEventListener ('change', () => 
+{
+    console.log('Изменение положения ползунка')
+    let value = document.querySelector(".input-range") .value;
+    let imgBefore = document.querySelector(".comparison__image-before");
+    let imgAfter = document.querySelector(".comparison__image-after");
+    imgBefore.style.width = parseInt(value) + '%';
+    imgAfter.style.width = (100 - parseInt(value)) + '%';   
+    beforeRangeValue = value;
+});
+
