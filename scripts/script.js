08edcd8be3ab73afe4456f03f80ffdd3b2fd1aeb
LyubovM11 <mazurovalubov8@gmail.com>
2025-04-11 11:17:34 +0300

    Add files via upload
    1) Добавили модальное окно "Оставить заявку"
    2) Сформировали массив из секции "Как это работает?"

--- scripts/script.js
+++ scripts/script.js
@@ -28,3 +28,61 @@ catRange.addEventListener ('change', () =>
     beforeRangeValue = value;
 });
 
+//Объявляем переменную welcоmeButtonModal и сохраняем в нее кнопку c классом welcome__button
+const welcоmeButtonModal = document.querySelector(".welcome__button");
+//объявляем переменную modalApplication и сохраняем в нее модальное окно, которое хотим увидеть
+const modalApplication = document.querySelector(".applications");
+
+//Если есть такая кнопка и модальное окно
+if (welcоmeButtonModal && modalApplication) {
+//Для кнопки «Записаться на курс» добавляем обработчик события клика по этой кнопке:
+    welcоmeButtonModal.addEventListener("click", () => {
+// удаляем атрибут hidden у модального окна modalApplication и модальное окно становится видимым
+      modalApplication.removeAttribute("hidden");
+    });
+}
+
+// добавляем обработчик события при клике вне области формы. Тогда каждый раз, когда пользователь кликает где-либо на фоне вокруг появившейся формы, будет вызываться функция,
+window.addEventListener("click", (event) => {
+// проверяем, был ли клик на фоне модального окна
+    if (event.target === modalApplication) {
+//если условие выполняется, добавляем атрибут hidden у модального окна modalApplication и модальное окно становится невидимым
+        modalApplication.setAttribute("hidden", true);
+    }
+});
+
+//Объявляем переменную closeModalButton и сохраняем в нее кнопку c классом application__button
+const closeModalButton = document.querySelector(".application__close");
+
+//Для кнопки «Закрыть» добавляем обработчик события клика по этой кнопке:
+closeModalButton.addEventListener("click", () => {
+// Добавляем атрибут hidden у модального окна modalApplication и модальное окно становится невидимым
+    modalApplication.setAttribute("hidden", true);
+});
+
+
+//Объявляем переменную AboutProductContainer и сохраняем в нее элементы about-product
+const AboutProductContainer = document.querySelector(".about-product__card");
+
+//  проверяем существует ли элемент AboutProductContainer, если он существует то переходим далее
+    if (AboutProductContainer) {
+//далее создаем массив dataTitleAboutProduct, который содержит строки с именами тренеров.(здесь уже пишем те значения, которые надо подставить вместо слова Тренер 1, Тренер 2 и т.д)
+        const dataTitleAboutProduct = [
+            "1. Функциональное питание содержит только полезные питательные вещества",
+            "2. Выпускается в виде порошка, который необходимо лишь залить кипятком",
+            "3. Замените один или два приема обычного корма на наше функциональное питание",
+            "4. Уже через месяц наслаждайтесь изменением к лучшему вашего питомца!",
+        ];
+
+//Объявляем переменную title.AboutProduct и сохраняем в нее все элементы на странице с классом treners__subtitle (где должны стоять имена тренеров)
+        const titleAboutProduct =
+        AboutProductContainer.querySelectorAll(".about-product__text");
+
+// Проходим по каждому элементу массива titleTreners с помощью цикла forEach. Внутри функции 2 переменные: item – текущий заголовок, а index — его индекс в массиве.
+titleAboutProduct.forEach((item, index) => {
+
+//здесь обновляем значение текущего заголовка (textContent) на новое значение из массива dataTitleCards, используя индекс текущего заголовка.
+            item.textContent = dataTitleAboutProduct [index];
+           });
+    }
+
